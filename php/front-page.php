3f413d5b7d6cd1a5bca261b0fa0cf3c47158ff7c
EN.WORKS <45923194+enyamahiroshi@users.noreply.github.com>
2024-01-26 09:09:21 +0900

    fp

--- php/front-page.php
+++ php/front-page.php
@@ -7,7 +7,7 @@
         <figure class="mv__catchcopy__image">
           <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/home/img-mv-catchcopy.svg" alt="">
         </figure>
-        <h1 class="mv__catchcopy__text">創業1948年以来、<br>私たちモモセボデーは常にお客様の<br class="sp">カーライフサポートに徹してきました</h1>
+        <h1 class="mv__catchcopy__text">創業148年以来、<br>私たちモモセボデーは常にお客様の<br class="sp">カーライフサポートに徹してきました</h1>
       </div>
     </div>
 
